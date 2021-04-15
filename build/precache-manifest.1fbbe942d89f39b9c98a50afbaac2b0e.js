56ccdf9b2befc3649eb24d00a45ed3e139bd84fb
Anmolnoor <anmolnoor59@gmail.com>
2021-04-15 23:01:30 +0530

    build

---
+++ build/precache-manifest.1fbbe942d89f39b9c98a50afbaac2b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7625cd6c1c2a5c0fc95aea7f1683a4da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c9863482cc13522f83b",
+    "url": "/static/js/2.302bc12e.chunk.js"
+  },
+  {
+    "revision": "2a3a23bf878d7187dc1e20049a44e9ba",
+    "url": "/static/js/2.302bc12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e3e859fa0f80b3c149",
+    "url": "/static/js/main.5d696e18.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
